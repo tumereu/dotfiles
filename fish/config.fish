4c0e55e9826782a9ea52c3c1ade00cf35a190d65
Tuomas Reunanen <tuomas.reunanen@optiscangroup.com>
2021-02-07 10:36:04 +0200

    Modified .dotfiles echo annoyance

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,4 @@
 # Check if the dotfiles directory is dirty!
-if test (git status $HOME/.dotfiles 2> /dev/null | tail -n1) != "nothing to commit (working directory clean)"
+if test (git -C $HOME/.dotfiles status 2> /dev/null | tail -n1) != "nothing to commit (working directory clean)"
     echo "Uncommitted changes in .dotfiles!"
 end
