ae05850093599ad2e69a50b6e785306dfe85d672
Tuomas Reunanen <tuomas.reunanen@optiscangroup.com>
2021-02-07 12:27:46 +0200

    Added /opt/bin to user path

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,7 @@
 # Check if the dotfiles directory is dirty!
-if test (git -C $HOME/.dotfiles status 2> /dev/null | tail -n1) != "nothing to commit, working tree clean"
+if test (git -C $HOME/.dotfiles status 2> /dev/null | tail -n1) != "nothing to commit, working tree cleanr"
     echo "Uncommitted changes in .dotfiles!"
 end
+
+# Add /opt/bin to PATH
+ contains /opt/bin $fish_user_paths; or set -Ua fish_user_paths /opt/bin
