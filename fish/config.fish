16fb8f282ab1a171d94c5b22e6d5b764a0a40e76
Tuomas Reunanen <tuomas.reunanen@optiscangroup.com>
2021-02-12 17:53:38 +0200

    Fixed a typo in config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,5 @@
 # Check if the dotfiles directory is dirty!
-if test (git -C $HOME/.dotfiles status 2> /dev/null | tail -n1) != "nothing to commit, working tree cleanr"
+if test (git -C $HOME/.dotfiles status 2> /dev/null | tail -n1) != "nothing to commit, working tree clean"
     echo "Uncommitted changes in .dotfiles!"
 end
 
