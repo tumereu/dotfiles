acba048f12c421488903509498458fbcbf0c44d3
Tuomas Reunanen <tuomas.reunanen@optiscangroup.com>
2021-05-05 07:25:53 +0300

    Changed ideavimrc, added maven aliases

--- fish/config.fish
+++ fish/config.fish
@@ -10,6 +10,9 @@ contains /opt/bin $fish_user_paths; or set -Ua fish_user_paths /opt/bin
 # Add .dotfiles/bin to PATH
 contains $HOME/.dotfiles/bin $fish_user_paths; or set -Ua fish_user_paths $HOME/.dotfiles/bin
 
+# Use Jdk8 as the default java home
+set -Ux JAVA_HOME /opt/jdk/jdk8u282-b08/
+
 # Aliases the java/javac-commands into something which uses the currently selected
 # JAVA_HOME environment variable. Autoloading functions do not seem to work for these,
 # for one reason or another.
